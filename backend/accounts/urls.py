3d94788cb88becb32d5583b69db233c2f2c1c245
Sugyeong <onmay16@gmail.com>
2022-02-10 22:32:49 -0600

    add: main page

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin),
     path('signout/', views.signout),
+    path('main/', views.mainpage),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,13 +1,16 @@
 import re
+import json
+from random import randint
 
 from django.http import JsonResponse
 from django.contrib.auth import authenticate, login, logout
+from django.test import tag
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import Follow, User, Profile
 from .serializers import ProfileSerializer, UserSerializer
 
-from playlist.models import Playlist
+from playlist.models import Playlist, Song, Tag
 from playlist.serializers import PlaylistSerializer
 
 from rest_framework.parsers import JSONParser
@@ -76,8 +79,92 @@ def signout(request):
         request.user.auth_token.delete()
         logout(request)
 
-
         return JsonResponse({'message':('See you Soon, ' + nickname)}, status=200)
+
+@permission_classes([IsAuthenticated])
+def mainpage(request):
+
+    def get_random_idx(obj, start):
+        total = obj.objects.all().count()
+        print('total: ' + str(total))
+        random_idx = randint(start, total)
+        return random_idx
+    
+    def tag_dic(tag_lst, dic={}):
+        for tag in tag_lst:
+            if tag.tag in dic:
+                dic[tag.tag] =+ 1
+            else:
+                dic[tag.tag] = 1
+        return dic
+
+    def tag_lst(songs, lst=[]):
+        for song in songs:
+            tags = Tag.objects.select_related().filter(song=song)
+            lst.extend(tags)
+        return lst
+    
+    def song_list(playlists, lst=[]):
+        for playlist in playlists:
+            songs = Song.objects.select_related().filter(playlist=playlist)
+            lst.extend(songs)
+        return lst
+
+    if request.method == 'GET':
+        user = request.user
+        nickname = Profile.objects.get(user=user).nickname
+        
+        # user recommendation section
+        first_user_idx = get_random_idx(User, 2)
+        second_user_idx = get_random_idx(User, 2)
+
+        while ((first_user_idx == user.id)):
+            first_user_idx = get_random_idx(User, 2)
+        while ((first_user_idx == second_user_idx) or (second_user_idx == user.id)):
+            second_user_idx = get_random_idx(User, 2)
+
+        first_user = User.objects.get(id=first_user_idx)
+        first_profile = Profile.objects.get(user=first_user)
+        first_playlists = Playlist.objects.filter(created_by=first_user)
+        first_tags = tag_dic(tag_lst(song_list(first_playlists)))
+
+        second_user = User.objects.get(id=second_user_idx)
+        second_profile = Profile.objects.get(user=second_user)
+        second_playlists = Playlist.objects.filter(created_by=second_user)
+        second_tags = tag_dic(tag_lst(song_list(second_playlists)))
+
+        # tag recommendation section
+        first_tag_idx = get_random_idx(Tag, 1)
+        second_tag_idx = get_random_idx(Tag, 1)
+
+        while ((first_tag_idx == second_tag_idx)):
+            second_tag_idx = get_random_idx(Tag, 1)
+        
+        first_tag = Tag.objects.get(id=first_tag_idx)
+        second_tag = Tag.objects.get(id=second_tag_idx)
+        
+        # Playlist recommendation section
+        first_playlist_idx = get_random_idx(Playlist, 1)
+        second_playlist_idx = get_random_idx(Playlist, 1)
+
+        while ((first_playlist_idx == second_playlist_idx)):
+            second_playlist_idx = get_random_idx(Playlist, 1)
+
+        first_playlist = Playlist.objects.get(id=first_playlist_idx)
+        second_playlist = Playlist.objects.get(id=second_playlist_idx)
+
+        return JsonResponse({
+            'First user': UserSerializer(first_user).data,
+            'First user\'s profile': ProfileSerializer(first_profile).data,
+            'First user\'s tags': json.dumps(first_tags),
+            'Second user': UserSerializer(second_user).data,
+            'Second user\'s profile': ProfileSerializer(second_profile).data,
+            'Second user\'s tags': json.dumps(second_tags),
+            'First tag': first_tag.tag,
+            'Second tag': second_tag.tag,
+            'First_playlist': PlaylistSerializer(first_playlist).data,
+            'Second_playlist': PlaylistSerializer(second_playlist).data
+        })
 
 @permission_classes([IsAuthenticated])
 def profile(request, nickname):
