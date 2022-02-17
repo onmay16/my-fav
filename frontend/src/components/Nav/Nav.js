0e2976e6a9348824ae63e005147d7899bfb73fb9
Sugyeong <onmay16@gmail.com>
2022-02-16 19:04:50 -0600

    add: Navigation component

---
+++ frontend/src/components/Nav/Nav.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import './Nav.css'
+
+import chart from '../../svg/chart.svg'
+import settings from '../../svg/settings.svg'
+import signout from '../../svg/signout.svg'
+
+function Nav() {
+
+    let navigate = useNavigate();
+    function handleClick(url) {
+        navigate(url)
+    }
+
+    return (
+        <div className='nav-bar'>
+            <div className='logo-white' onClick={handleClick('/main')}> 
+            {/* should change route depending signin status */}
+                MY
+                <br />
+                FAV
+            </div>
+            <div className='icons'>
+                <img className='icon-chart' src={chart} onClick={handleClick('/report')} />
+                <br />
+                <img className='icon-settings' src={settings} onClick={handleClick('/settings')} />
+                <br />
+                <img className='signout' src={signout} onClick={handleClick('/splash')} /> 
+                {/* add logout action */}
+            </div>
+        </div>
+    )
+}
+
+export default Nav
