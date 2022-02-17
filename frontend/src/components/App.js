72b8054491375f5785958b1225716b41160beecb
Sugyeong <onmay16@gmail.com>
2022-02-16 19:05:48 -0600

    add: routes for new page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "../pages/Signup/Signup.js";
 import Splash from '../pages/Splash/Splash.js';
 import Signin from "./Modals/Signin.js";
+import Main from "../pages/Main/Main.js";
+import Nav from "./Nav/Nav.js";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Splash />} />
         <Route path="/signup" element={<Signup />} /> 
         <Route path="/signin" element={<Signin />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/nav" element={<Nav />} />
       </Routes>
     </Router>
   );
