6b2e51c428b8e224df1893953aa4b31ec82ca2bc
Sugyeong <onmay16@gmail.com>
2022-02-17 00:53:56 -0600

    update: main page

--- frontend/src/pages/Main/Main.js
+++ frontend/src/pages/Main/Main.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import './Main.css'
 
 import Nav from '../../components/Nav/Nav';
 
+import message from '../../svg/message.svg'
+import notification from '../../svg/notification.svg'
+import profile from '../../svg/profile.svg'
+
 function Main() {
+
+    const [user, userChange] = useState('')
+    
     return (
-        <div>
-            <Nav />
+        <div className='main'>
+            <Nav className='nav' />
+            <div className='wrapper'>
+                <div className='header'>
+                    <div className='search-box'>
+                        <div className='search-filter'></div>
+                        <input className='search-input' type='text' placeholder='       Search with song title, artist name...' />
+                    </div>
+                    <div className='personal-menu'>
+                        <img className='message' src={message} />
+                        <img className='notification' src={notification} />
+                        <img className='profile' src={profile} />
+                    </div>
+                </div>
+                <div className='content-section'>
+                    <div className='welcome-comment'>
+                        Welcome Back
+                        <br />
+                        Hong!
+                    </div>
+                    <div className='recommendation'>
+                        <div className='first-column'>
+                            <div className='user-recommendation'></div>
+                            <div className='genre-recommendation'></div>
+                        </div>
+                        <div className='second-column'>
+                            <div className='playlist-recommendation'></div>
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
