1be4648ceb2dd049b84dbf6e2257179ac618d23b
Sugyeong <onmay16@gmail.com>
2022-06-03 14:22:59 -0500

    update: lastfm REST APIs screen pages not only the first page

--- backend/playlist/lastfm.py
+++ backend/playlist/lastfm.py
@@ -1,3 +1,4 @@
+from unittest import result
 import requests
 import json
 from django.http import JsonResponse
@@ -8,8 +9,46 @@ from backend import my_settings
 api_url = 'http://ws.audioscrobbler.com/2.0/'
 api_key = my_settings.LASTFM_API_KEY
 
+def get_result(url):
+    result_data = requests.get(url).text
+    fine_data = json.loads(result_data)
+    return fine_data
+
+def search_track(track):
+
+    fine_data = get_result(f'{api_url}?method=track.search&format=json&track={track}&api_key={api_key}')
+
+    total_results = fine_data['results']['opensearch:totalResults']
+    items_per_page = fine_data['results']['opensearch:itemsPerPage']
+    total_pages = int(total_results)//int(items_per_page) + 1
+
+    # print(total_pages)
+
+    # fine_data = get_result(f'{api_url}?method=track.search&format=json&track={track}&page=3&api_key={api_key}')
+
+    # print(fine_data)
+
+    tracks = {}
+    for pg in range(1, total_pages):
+        fine_data = get_result(f'{api_url}?method=track.search&format=json&track={track}&page={pg}&api_key={api_key}')
+
+        print(fine_data)
+
+        track_data = fine_data['results']['trackmatches']['track']
+
+        for track in track_data:
+            name = track['name']
+            tracks[name] = {'artist':'', 'url':'', 'mbid':''}
+            tracks[name]['artist'] = track['artist']
+            tracks[name]['url'] = track['url']
+            tracks[name]['mbid'] = track['mbid']
+
+    # return JsonResponse({'tracks': tracks})
+    return tracks
+    
+
 @csrf_exempt
-def search_artist(request, artist):
+def search_artist(artist):
     url = api_url+'?method=artist.search&format=json&artist='+artist+'&api_key='+api_key
     result_data = requests.get(url).text
     fine_data = json.loads(result_data)
@@ -22,43 +61,10 @@ def search_artist(request, artist):
         artists[name]['mbid'] = artist['mbid']
         artists[name]['url'] = artist['url']
 
-    return JsonResponse({'artists': artists})
+    # return JsonResponse({'artists': artists})
+    return artists
 
 
-def search_track(request, track):
-    url = api_url+'?method=track.search&format=json&track='+track+'&api_key='+api_key
-    
-    result_data = requests.get(url).text
-    fine_data = json.loads(result_data)
-    track_data = fine_data['results']['trackmatches']['track']
-
-    tracks = {}
-    for track in track_data:
-        name = track['name']
-        tracks[name] = {'artist':'', 'url':'', 'mbid':''}
-        tracks[name]['artist'] = track['artist']
-        tracks[name]['url'] = track['url']
-        tracks[name]['mbid'] = track['mbid']
-
-    return JsonResponse({'tracks': tracks})
-    
-
-def search_track(request, track):
-    url = api_url+'?method=track.search&format=json&track='+track+'&api_key='+api_key
-    
-    result_data = requests.get(url).text
-    fine_data = json.loads(result_data)
-    track_data = fine_data['results']['trackmatches']['track']
-
-    tracks = {}
-    for track in track_data:
-        name = track['name']
-        tracks[name] = {'artist':'', 'url':'', 'mbid':''}
-        tracks[name]['artist'] = track['artist']
-        tracks[name]['url'] = track['url']
-        tracks[name]['mbid'] = track['mbid']
-
-    return JsonResponse({'tracks': tracks})
 
 
 def getInfo_track(request, artist, track):
