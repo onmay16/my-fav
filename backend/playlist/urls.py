df37c41c2ecf3a7da04485f0021e4792df250219
Sugyeong <onmay16@gmail.com>
2022-06-03 14:24:38 -0500

    update: urls

--- backend/playlist/urls.py
+++ backend/playlist/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from . import views, lastfm
+from . import views, lastfm, playlist, search
 
 urlpatterns = [
     path('main/', views.mainpage),
-    path('search/artist/<str:artist>/', lastfm.search_artist),
+    # path('search/artist/<str:artist>/', lastfm.search_artist),
     path('search/track/<str:track>/', lastfm.search_track),
     path('info/<str:artist>/<str:track>/', lastfm.getInfo_track),
-    path('add/', views.add_playlist),
-    path('edit/<int:id>/', views.edit_playlist),
+    path('add/', playlist.add_playlist),
+    path('edit/<int:id>/', playlist.edit_playlist),
+    path('search/', search.search),
 ]
