5eb1e7b4063395975a90e00e9ea0ffeb1be58b5a
Sugyeong <onmay16@gmail.com>
2022-06-03 14:25:01 -0500

    update: move functions that are not for view to other .py files

--- backend/playlist/views.py
+++ backend/playlist/views.py
@@ -1,5 +1,4 @@
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 
 from random import randint
 
@@ -7,8 +6,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes
 from rest_framework.parsers import JSONParser
 
-from .models import Playlist, Song
-from .serializers import PlaylistSerializer
+from .models import Song
+
 from accounts.models import User, Profile
 from accounts.serializers import UserSerializer, ProfileSerializer
 
@@ -80,69 +79,3 @@ def mainpage(request):
             # 'First_playlist': PlaylistSerializer(first_playlist).data,
             # 'Second_playlist': PlaylistSerializer(second_playlist).data
         })
-
-@csrf_exempt
-@permission_classes([IsAuthenticated])
-def add_playlist(request):
-    if request.method == 'POST':
-        
-        data = JSONParser().parse(request)
-
-        new_playlist = Playlist()
-        
-        title = data['title']
-        jacket= data.get('jacket', None)
-        description = data.get('description', None)
-        is_private = data.get('is_private', None)
-
-        new_playlist.created_by = request.user
-        if not title or title.isspace():
-            return JsonResponse({'message':'Please title your playlist.'})
-        new_playlist.title = title
-        if jacket != None:
-            new_playlist.jacket_pic = jacket
-        if description != None:
-            new_playlist.description = description
-        if is_private != None:
-            new_playlist.is_private = is_private
-
-        new_playlist.save()
-        return JsonResponse({'message':'New playlist is successfully added.', 'added playlist':PlaylistSerializer(new_playlist).data})
-
-@csrf_exempt
-@permission_classes([IsAuthenticated])
-def edit_playlist(request, id):
-    playlist = Playlist.objects.get(id=id)
-    if request.method == 'PUT':
-        
-        data = JSONParser().parse(request)
-
-        title = data.get('title', None)
-        jacket= data.get('jacket', None)
-        description = data.get('description', None)
-        is_private = data.get('is_private', None)
-
-        if title != None:
-            playlist.title = title
-        if jacket != None:
-            playlist.jacket_pic = jacket
-        if description != None:
-            playlist.description = description
-        if is_private != None:
-            playlist.is_private = is_private
-
-        playlist.save()
-        return JsonResponse({'messsage':'Playlist has been successfully updated.'})
-
-    elif request.method == 'DELETE':
-
-        playlist.delete()
-        return JsonResponse({'messsage':'Playlist has been successfully deleted.'})
-
-
-
-    
-    
-    
-    
-    
