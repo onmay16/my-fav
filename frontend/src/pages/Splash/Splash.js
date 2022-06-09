8a87a23954ebc1fe44ed1435bab788616118cd12
Sugyeong <onmay16@gmail.com>
2022-06-09 15:37:20 -0500

    update new splash page

--- frontend/src/pages/Splash/Splash.js
+++ frontend/src/pages/Splash/Splash.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 
+import logo from '../../svg/big-logo.svg';
+
 import './Splash.css'
 
 import LogoRed from '../../components/LogoRed/LogoRed.js';
@@ -15,15 +17,17 @@ function Splash() {
     }
 
     return (
-        <div>
-            <header>
-                <LogoRed />
-                <button className='join' onClick={handleClick}>sharing now!</button>
+        <div className='entire-splash-page'>
+            <header className='splash-header'>
+                {/* <LogoRed />
+                <button className='join' onClick={handleClick}>sharing now!</button> */}
+                <marquee behavior="" direction="">curate your own playlist and share. find other’s taste. CURATE YOUR PLAYLIST AND SHARE. FIND OTHER'S TASTE.</marquee>
             </header>
-            <body>
-                <h1>This is Splash page</h1>
+            <body className='splash-body'>
+                {/* <h1>This is Splash page</h1> */}
+                <img src={logo} alt="curave_logo" className='logo' />
             </body>
-            <Footer />
+            {/* <Footer /> */}
         </div>
     );
 }
