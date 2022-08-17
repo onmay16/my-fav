5aeafd4f6ed7b059eff75831cda1d11145e8589f
Sugyeong <onmay16@gmail.com>
2022-08-17 17:41:43 -0500

    update search function

--- backend/playlist/search.py
+++ backend/playlist/search.py
@@ -1,16 +1,22 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import JSONParser
 
 from .lastfm import search_artist, search_track
 
-@permission_classes([IsAuthenticated])
+@csrf_exempt
+# @permission_classes([IsAuthenticated])
 def search(request):
-    data = JSONParser().parse(request)
-    category = data['category']
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        category = data['category']
 
-    if category == 'artist':
-        return search_artist(data['artist'])
-    else:
-        # return search_track(data['title'])
-        return search_track(data['title'])
+        if category == 'artist':
+            # return search_artist(data['word'])
+            return JsonResponse(search_artist(data['word']))
+        else:
+            return JsonResponse(search_track(data['word']))
+            # return search_track(data['word'])
