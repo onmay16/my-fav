31bae1ad1578b190bccd0a7b3d64e3a48b567f6c
Sugyeong <onmay16@gmail.com>
2023-04-04 22:21:28 -0500

    main: change settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,7 +147,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     # Tell Django where to look for React's static files (css, js)
     # os.path.join(BASE_DIR, 'static',),
-    os.path.join(BASE_DIR, './client/static'),
+    os.path.join(BASE_DIR, 'client/static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
